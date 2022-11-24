86c7426a2ff2cb83d1df39fc86593bb23cc480f7
Newadventure2022 <117087991+Newadventure2022@users.noreply.github.com>
2022-11-24 14:18:44 +0100

    Updated all of the Javascript

--- code.js
+++ code.js
@@ -21,17 +21,19 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayTemperature(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let cityElement = document.querySelector("#current-city");
-  let descriptionElement = document.querySelector("#weather");
+function displayWeatherCondition(response) {
+  let currentTemp = document.querySelector("#temperature");
+  let currentLocation = document.querySelector("#current-city");
+  let temperature = Math.round(response.data.temperature.current);
+  let weatherElement = document.querySelector("#weather");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
-  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
-  cityElement.innerHTML = response.data.city;
-  descriptionElement.innerHTML = response.data.condition.description;
+
+  currentLocation.innerHTML = `${response.data.city}`;
+  currentTemp.innerHTML = `${temperature}`;
+  weatherElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.time * 1000);
@@ -40,13 +42,96 @@ function displayTemperature(response) {
     `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
   );
   iconElement.setAttribute("alt", response.data.condition.description);
-
-  console.log(response.data);
 }
 
-let apiKey = "o8145f06881d09401ad03t8b0f09e589";
-let city = "Paris";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+function searchLocation(position) {
+  let apiKey = "o8145f06881d09401ad03t8b0f09e589";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=${lat}&lon=${lon}&key=${apiKey}&units=metric`;
 
-console.log(apiUrl);
-axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input-result");
+  let currentCity = document.querySelector("#current-city");
+  if (searchInput.value) {
+    currentCity.innerHTML = `${searchInput.value}`;
+    search(searchInput.value);
+  } else {
+    currentCity.innerHTML = null;
+    alert("Please type a city");
+  }
+}
+
+function showTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let city = document.querySelector("#current-city");
+  let weatherElement = document.querySelector("#weather");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  city.innerHTML = response.data.city;
+  weatherElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+  dateElement.innerHTML = formatDate(response.data.time * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+}
+function search(city) {
+  let apiKey = "o8145f06881d09401ad03t8b0f09e589";
+  let units = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+}
+function handleCity(e) {
+  e.preventDefault();
+  let searchInput = document.querySelector("#search-input-result");
+  search(searchInput.value);
+}
+function fahrenheitTemp(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector("#temperature");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function celsiusTemp(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", fahrenheitTemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", celsiusTemp);
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleCity);
+
+search("Paris");
